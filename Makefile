b4a6813532148e951d1a1ee9569d97e6a6a86868
Lucas van Oosterhout <lucasvanoosterhout@gmail.com>
2017-12-02 19:31:06 +0100

    area frame allocator of Phill is weird and not really all that wel implemented... make one myself!

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 arch ?= x86_64
 kernel := build/kernel-$(arch).bin
 iso := build/os-$(arch).iso
+target ?= $(arch)-blog_os
+rust_os := target/$(target)/debug/librust_os.a
 
 linker_script := src/arch/$(arch)/linker.ld
 grub_cfg := src/arch/$(arch)/grub.cfg
@@ -8,10 +10,7 @@ assembly_source_files := $(wildcard src/arch/$(arch)/*.asm)
 assembly_object_files := $(patsubst src/arch/$(arch)/%.asm, \
 	build/arch/$(arch)/%.o, $(assembly_source_files))
 
-target ?= $(arch)-unknown-linux-gnu
-rust_os := target/$(target)/debug/librust_os.a
-
-.PHONY: all clean run iso
+.PHONY: all clean run iso kernel
 
 all: $(kernel)
 
@@ -30,13 +29,12 @@ $(iso): $(kernel) $(grub_cfg)
 	@grub-mkrescue -o $(iso) build/isofiles 2> /dev/null
 	@rm -r build/isofiles
 
-$(kernel): cargo $(rust_os) $(assembly_object_files) $(linker_script)
-	@ld -n -gc-sections -T $(linker_script) -o $(kernel) \
-	$(assembly_object_files) $(rust_os)
+$(kernel): kernel $(rust_os) $(assembly_object_files) $(linker_script)
+	@ld -n --gc-sections -T $(linker_script) -o $(kernel) \
+		$(assembly_object_files) $(rust_os)
 
-
-cargo:
-	@cargo build --target $(target)
+kernel:
+	@xargo build --target $(target)
 
 # compile assembly files
 build/arch/$(arch)/%.o: src/arch/$(arch)/%.asm
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,64 @@
-#![feature(lang_items, const_fn, unique)]
+
+#![feature(lang_items, const_fn, unique, const_unique_new)]
 #![no_std]
+
+#[macro_use]
+mod vga_buffer;
+mod memory;
+
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
-
-mod vga_buffer;
-
+extern crate multiboot2;
 
 
 #[no_mangle]
-pub extern fn rust_main() {
-	use core::fmt::Write;
-	vga_buffer::WRITER.lock().write_str("Hello Again!");
-	write!(vga_buffer::WRITER.lock(), ", some numbers: \n\n\n\n{} {}", 42, 3.1415);
-	loop {}
-}
+pub extern fn rust_main(multiboot_information_address: usize) {
 
-#[allow(non_snake_case)]
-#[no_mangle]
-pub extern "C" fn _Unwind_Resume() -> ! {
-	loop {}
-}
+	vga_buffer::clear_screen();
 
+	let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+	let memory_map_tag = boot_info.memory_map_tag()
+		.expect("Memory Tag Required");
+	println!("memory Areas");
+	for area in memory_map_tag.memory_areas() {
+		println!("    start: 0x{:x}, length: 0x{:x}",
+		   area.base_addr, area.length);
+	}
+
+	let elf_sections_tag = boot_info.elf_sections_tag()
+    .expect("Elf-sections tag required");
+
+	println!("kernel sections:");
+	for section in elf_sections_tag.sections() {
+	    println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+	        section.addr, section.size, section.flags);
+	}
+
+	let kernel_start = elf_sections_tag.sections().map( |s| s.addr)
+		.min().unwrap();
+	let kernel_end = elf_sections_tag.sections().map(|s| s.addr)
+		.max().unwrap();
+	let multiboot_start = multiboot_information_address;
+	let multiboot_end 	= multiboot_start + (boot_info.total_size as usize);
+
+	println!("kernel start: 0x{:x} kernel end: 0x{:x}, size: {} kbytes", kernel_start, kernel_end, (kernel_end - kernel_start)/1000);
+	println!("multiboot start: 0x{:x} multiboot end: 0x{:x}, size: {} bytes", multiboot_start, multiboot_end, multiboot_end - multiboot_start);
+
+	let mut frame_allocator = memory::AreaFrameAllocator::new(
+	    kernel_start as usize, kernel_end as usize, multiboot_start,
+	    multiboot_end, memory_map_tag.memory_areas());
+		
+		
+
+}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
+#[lang = "panic_fmt"] 
+#[no_mangle] 
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+	println!("\n\nPANIC in {} at line {}", file, line);
+	println!("    {}", fmt);
+	loop{}
+}
+
